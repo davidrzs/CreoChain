ec07a41836da502172433f1046d8d56e587e0353
David Zollikofer <david@zollikofer.io>
2018-06-11 12:59:46 +0200

    added dummy functions

--- chain/chainManipulation.go
+++ chain/chainManipulation.go
@@ -2,21 +2,42 @@ package chain
 
 import "fmt"
 
-//Test ...
-
 // CheckIfChainExists checks if a chain with a given name can be found in our database
 func CheckIfChainExists(name string) bool {
 	return false
 }
 
+// GetWholeChain returns a pointer to a blockchain loaded from the storage.
 func GetWholeChain(name string) *Blockchain {
 	return nil
 }
 
+// GetBlockOfChain gets a specific block of a specific chain
 func GetBlockOfChain() *Block {
 	return nil
 }
 
+//StoreBlockChain ..
+func StoreBlockChain(chain *Blockchain, name string) error {
+	return nil
+}
+
+//AddBlockToChain ..
+func AddBlockToChain() error {
+	return nil
+}
+
+// DeleteBlockChain deletes an entire blockchain from the database. Use with caution.
+func DeleteBlockChain(name string) error {
+	return nil
+}
+
+// DeleteBlockFromChain should NEVER be used since it would mess up the hashes, only useful for debugging.
+func DeleteBlockFromChain() error {
+	return nil
+}
+
+//Test ...
 func Test() {
 	bc := NewBlockchain("test")
 

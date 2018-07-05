5d0a8808f7719d8ea5a13d7571c508212785c646
David Zollikofer <david@zollikofer.co>
2018-07-05 20:42:28 +0200

    adding file

--- chain/blockchain.go
+++ chain/blockchain.go
@@ -1,12 +1,16 @@
 package chain
+package main
 
 import (
 	"bytes"
 	"crypto/sha256"
+	b64 "encoding/base64"
 	"fmt"
+	"runtime"
 	"strconv"
+	"strings"
 	"time"
-    "runtime"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 )
@@ -56,12 +60,12 @@ func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", "")
 }
 
-func main() {
+func runChainTest() {
 
 	db := DbSetup("mysql", "david:password@/godb?charset=utf8&parseTime=True&loc=Local")
 	defer db.Close()
-	db.DropTableIfExists(&Block{})
-	db.DropTableIfExists(&Blockchain{})
+	//db.DropTableIfExists(&Block{})
+	//db.DropTableIfExists(&Chain{})
 	/*Blockchain1 := Chain{Name: "chain1", AccessToken: "aT", Blocks: []Block{
 		{Timestamp: time.Now().Unix(), Data: "block1", PrevBlockHash: "", Hash: ""},
 		{Timestamp: time.Now().Unix(), Data: "block2", PrevBlockHash: "", Hash: ""}}}
@@ -70,13 +74,13 @@ func main() {
 	*/
 	bchain := &Chain{}
 
-	//
+	fmt.Println(AddBlockToChain(db, "chain1", "aT", "this is some data"))
 
 	fmt.Println("bchain", bchain.ChainID)
 
 }
 
-//
+//AddBlockToChain adds a block to a blockchain by saving it correctly in the database.
 func AddBlockToChain(db *gorm.DB, chainName string, authCode string, data string) (bool, string) {
 
 	err := false
@@ -84,22 +88,37 @@ func AddBlockToChain(db *gorm.DB, chainName string, authCode string, data string
 
 	//find the chain we are looking for.
 	bchain := &Chain{}
-	db.Model(&bchain).Association("Blocks").Append(Block{Timestamp: time.Now().Unix(), Data: "block3", PrevBlockHash: "", Hash: ""})
 
 	if db.Where("name=?", chainName).First(&bchain).RecordNotFound() {
 		err = true
-		errString = errString += "No chain with the name " + chainName + " found. "
+		errString = "No chain with the name " + chainName + " found. "
 		return err, errString
 	}
+
+	//check if we are authorized to add a block
 	var trimmedAccessToken string
 	if runtime.GOOS == "windows" {
-		input = strings.TrimRight(authCode, "\r\n")
-	  } else {
-		input = strings.TrimRight(authCode, "\n")
-	  }
-	  
-	if bchain.AccessToken
+		trimmedAccessToken = strings.TrimRight(authCode, "\r\n")
+	} else {
+		trimmedAccessToken = strings.TrimRight(authCode, "\n")
+	}
 
+	if bchain.AccessToken != trimmedAccessToken {
+		err = true
+		errString = "AccessToken for chain " + chainName + " isn't correct. "
+		return err, errString
+	}
+
+	//creating the block
+	bchainBlocks := &[]Block{}
+	db.Model(&bchain).Association("Blocks").Find(bchainBlocks)
+	fmt.Println(bchainBlocks)
+	prevBlockHash := (*bchainBlocks)[len(*bchainBlocks)-1].Hash
+	blockToAdd := NewBlock(data, prevBlockHash)
+	blockToAdd.setHash()
+
+	//add it to the database
+	db.Model(&bchain).Association("Blocks").Append(blockToAdd)
 	return false, ""
 }
 
